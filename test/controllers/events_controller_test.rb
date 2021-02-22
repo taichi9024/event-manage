bf41008fd367c359f98c0ac9a8548de8f702adb0
tai <ebh48978@gmail.com>
2021-02-23 03:23:53 +0900

    test修正

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,13 +1,4 @@
 require "test_helper"
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
-  test "自分のイベントを削除 成功" do
-    owner = FactoryBot.create(:user)
-    event = FactoryBot.create(:event, user: owner)
-    sign_in_as(owner)
-    assert_difference("Event.count", -1) do
-      delete event_path(event)
-    end
-  end
-
 end
--- test/controllers/tickets_controller_test.rb
+++ test/controllers/tickets_controller_test.rb
@@ -1,8 +1,4 @@
 require "test_helper"
 
 class TicketsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get tickets_new_url
-    assert_response :success
-  end
 end
